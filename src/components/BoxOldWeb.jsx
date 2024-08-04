a2c94c65d6866762c99d2af9e005f35644701f16
SepuluhEnamX6 <163085628+SepuluhEnamX6@users.noreply.github.com>
2024-08-04 06:53:19 -0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="kodam.html">
+			<a href="https://sepuluhenamx6.github.io/cek-kodam/">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
