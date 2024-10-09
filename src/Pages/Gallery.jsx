34cc25754b0437c964335431f83128cd0ac6ecd5
SepuluhEnamX6 <163085628+SepuluhEnamX6@users.noreply.github.com>
2024-10-09 07:17:07 +0700

    Update Gallery.jsx

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -14,8 +14,6 @@ const Carousel = () => {
     "/foto1.jpg",
     "/foto2.jpg",
     "/foto3.jpg",
-    "/poster.jpg",
-    "/foto12.jpg",
     "/pengajian1.jpg
   ]);
   const [open, setOpen] = useState(false);
